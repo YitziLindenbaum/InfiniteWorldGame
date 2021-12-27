f5e8a5a06a70a42814477084259fc7adb85133b9
gal66 <gal.rosenblum@mail.huji.ac.il>
2021-12-27 18:24:50 +0200

    initial commit take 9 add Night.java

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -2,6 +2,7 @@ package pepse;
 
 import danogl.GameManager;
 import danogl.GameObject;
+import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
@@ -9,13 +10,15 @@ import danogl.gui.WindowController;
 import danogl.util.Vector2;
 import pepse.world.Block;
 import pepse.world.Sky;
+import pepse.world.daynight.Night;
 
 public class PepseGameManager extends GameManager {
 
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
-        Sky.create(gameObjects(),windowController.getWindowDimensions(),-50);
+        Sky.create(gameObjects(),windowController.getWindowDimensions(), Layer.BACKGROUND);
+        Night.create(gameObjects(), Layer.FOREGROUND, windowController.getWindowDimensions(), 30);
 
 
 

4c80f10fae5c443e635489e9a8a40ff4059c509d
chucklind <yitzchak.lindenbaum@mail.huji.ac.il>
2022-01-09 14:56:20 +0200

    Added documentation and switched order of methods in manager

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -46,6 +46,104 @@ public class PepseGameManager extends GameManager {
     private Vector2 windowDimensions;
 
     /**
+     * initializeGame - initialize all the objects in the game
+     * @param imageReader - image reader
+     * @param soundReader - sound reader
+     * @param inputListener - parse keys
+     * @param windowController object
+     */
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+                               UserInputListener inputListener,
+                               WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        windowDimensions = windowController.getWindowDimensions();
+
+        Sky.create(gameObjects(),windowDimensions, SKY_LAYER);
+
+        terrain = new Terrain(gameObjects(), GROUND_LAYER, windowDimensions, SEED);
+        terrain.createInRange(INIT_MIN_X, INIT_MAX_X);
+
+        Night.create(gameObjects(), NIGHT_LAYER, windowDimensions, CYCLE_LENGTH);
+
+        GameObject sun = Sun.create(gameObjects(), SUN_LAYER,
+                windowDimensions, CYCLE_LENGTH);
+
+        SunHalo.create(gameObjects(), SUN_HALO_LAYER, sun,
+                SUN_HALO_COLOR);
+
+        tree = new Tree(gameObjects(), terrain::groundHeightAt, TREE_LAYER, LEAVES_LAYER, SEED);
+        tree.createInRange(INIT_MIN_X, INIT_MAX_X);
+
+        initializeLayers();
+        initializeAvatar(imageReader, inputListener, windowController);
+
+    }
+
+    /**
+     * Initializes avatar and camera
+     * @param imageReader
+     * @param inputListener
+     * @param windowController
+     */
+    private void initializeAvatar(ImageReader imageReader, UserInputListener inputListener,
+                           WindowController windowController) {
+        //create avatar
+        float midX = windowDimensions.x() / 2;
+        float y =
+            (float) Math.floor(terrain.groundHeightAt(midX) / Block.SIZE) * Block.SIZE - Block.SIZE - Avatar.AVATAR_SIZE;
+        Vector2 initialAvatarLocation = new Vector2(midX, y);
+        avatar = Avatar.create(gameObjects(), AVATAR_LAYER, initialAvatarLocation,
+            inputListener, imageReader);
+
+        //set camera following after the avatar
+        Camera camera = new Camera(avatar, windowDimensions.mult(HALF).add(initialAvatarLocation.mult(MINUS)),
+                windowController.getWindowDimensions(),
+                windowController.getWindowDimensions());
+
+        setCamera(camera);
+    }
+
+    /**
+     * Initializes layers to ensure proper collisions.
+     */
+    private void initializeLayers() {
+        //set avatar collide with the ground and tree
+        gameObjects().layers().shouldLayersCollide(AVATAR_LAYER, GROUND_LAYER,true); //first layer
+        //gameObjects().layers().shouldLayersCollide(AVATAR_LAYER,Terrain.GROUND_LAYER, true);
+        gameObjects().layers().shouldLayersCollide(AVATAR_LAYER, TREE_LAYER,true);
+        //set leaves collide with rhe first layer of the ground
+        gameObjects().layers().shouldLayersCollide(LEAVES_LAYER, GROUND_LAYER, true);
+    }
+
+    /**
+     * Run once per frame.
+     * @param deltaTime -
+     */
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        //Real world - adding more trees and ground
+        //check if avatar get closer to the end of the ground
+        if (avatar.getCenter().x() + windowDimensions.x() > cur_maxX) {
+            terrain.createInRange(cur_maxX, cur_maxX + INIT_MAX_X);
+            tree.createInRange(cur_maxX, cur_maxX + INIT_MAX_X);
+            cur_maxX += INIT_MAX_X;
+            cur_minX -= INIT_MIN_X;
+            removeBesidesRange(cur_minX, cur_maxX); //remove the unnecessary objects
+        }
+        if (avatar.getCenter().x() - windowDimensions.x() < cur_minX) {
+            terrain.createInRange(cur_minX + INIT_MIN_X, cur_minX);
+            tree.createInRange(cur_minX + INIT_MIN_X, cur_minX);
+            cur_maxX -= INIT_MAX_X;
+            cur_minX += INIT_MIN_X;
+            removeBesidesRange(cur_minX, cur_maxX); //remove the unnecessary objects
+        }
+    }
+
+
+
+    /**
      * Responsible for remove all the objects that beside the range of the game
      * @param minX of range of the game
      * @param maxX of range of the game
@@ -79,93 +177,8 @@ public class PepseGameManager extends GameManager {
         }
     }
 
-    /**
-     * Run once per frame.
-     * @param deltaTime -
-     */
-    @Override
-    public void update(float deltaTime) {
-        super.update(deltaTime);
-        //Real world - adding more trees and ground
-        //check if avatar get closer to the end of the ground
-        if (avatar.getCenter().x() + windowDimensions.x() > cur_maxX) {
-            terrain.createInRange(cur_maxX, cur_maxX + INIT_MAX_X);
-            tree.createInRange(cur_maxX, cur_maxX + INIT_MAX_X);
-            cur_maxX += INIT_MAX_X;
-            cur_minX -= INIT_MIN_X;
-            removeBesidesRange(cur_minX, cur_maxX); //remove the unnecessary objects
-        }
-        if (avatar.getCenter().x() - windowDimensions.x() < cur_minX) {
-            terrain.createInRange(cur_minX + INIT_MIN_X, cur_minX);
-            tree.createInRange(cur_minX + INIT_MIN_X, cur_minX);
-            cur_maxX -= INIT_MAX_X;
-            cur_minX += INIT_MIN_X;
-            removeBesidesRange(cur_minX, cur_maxX); //remove the unnecessary objects
-        }
-    }
-
-    /**
-     * initializeGame - initialize all the objects in the game
-     * @param imageReader - image reader
-     * @param soundReader - sound reader
-     * @param inputListener - parse keys
-     * @param windowController -
-     */
-    @Override
-    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
-                               UserInputListener inputListener,
-                               WindowController windowController) {
-        super.initializeGame(imageReader, soundReader, inputListener, windowController);
-        windowDimensions = windowController.getWindowDimensions();
-
-        Sky.create(gameObjects(),windowDimensions, SKY_LAYER);
-
-        terrain = new Terrain(gameObjects(), GROUND_LAYER, windowDimensions, SEED);
-        terrain.createInRange(INIT_MIN_X, INIT_MAX_X);
-
-        Night.create(gameObjects(), NIGHT_LAYER, windowDimensions, CYCLE_LENGTH);
-
-        GameObject sun = Sun.create(gameObjects(), SUN_LAYER,
-                windowDimensions, CYCLE_LENGTH);
-
-        SunHalo.create(gameObjects(), SUN_HALO_LAYER, sun,
-                SUN_HALO_COLOR);
-
-        tree = new Tree(gameObjects(), terrain::groundHeightAt, TREE_LAYER, LEAVES_LAYER, SEED);
-        tree.createInRange(INIT_MIN_X, INIT_MAX_X);
-
-        initializeLayers();
-        initializeAvatar(imageReader, inputListener, windowController);
-
-    }
-
-    private void initializeAvatar(ImageReader imageReader, UserInputListener inputListener,
-                           WindowController windowController) {
-        //create avatar
-        float midX = windowDimensions.x() / 2;
-        float y =
-            (float) Math.floor(terrain.groundHeightAt(midX) / Block.SIZE) * Block.SIZE - Block.SIZE - Avatar.AVATAR_SIZE;
-        Vector2 initialAvatarLocation = new Vector2(midX, y);
-        avatar = Avatar.create(gameObjects(), AVATAR_LAYER, initialAvatarLocation,
-            inputListener, imageReader);
 
 
-        //set camera following after the avatar
-        Camera camera = new Camera(avatar, windowDimensions.mult(HALF).add(initialAvatarLocation.mult(MINUS)),
-                windowController.getWindowDimensions(),
-                windowController.getWindowDimensions());
-
-        setCamera(camera);
-    }
-
-    private void initializeLayers() {
-        //set avatar collide with the ground and tree
-        gameObjects().layers().shouldLayersCollide(AVATAR_LAYER, GROUND_LAYER,true); //first layer
-        //gameObjects().layers().shouldLayersCollide(AVATAR_LAYER,Terrain.GROUND_LAYER, true);
-        gameObjects().layers().shouldLayersCollide(AVATAR_LAYER, TREE_LAYER,true);
-        //set leaves collide with rhe first layer of the ground
-        gameObjects().layers().shouldLayersCollide(LEAVES_LAYER, GROUND_LAYER, true);
-    }
 
     /**
      * Main funtion to run program.
